f9483acbb1d620c81193087acf300a7b11753291
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-04-29 13:27:06 -0300

    Criação da Classe Pokemon

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -11,6 +11,59 @@ package model;
  */
 public class Pokemon {
     String nome;
+    String anterior;
+    String proximo;
     int ID;
     Tipo tipo;
+
+    public Pokemon() {
+    }
+
+    public Pokemon(String nome, String anterior, String proximo, int ID, Tipo tipo) {
+        this.nome = nome;
+        this.anterior = anterior;
+        this.proximo = proximo;
+        this.ID = ID;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(String anterior) {
+        this.anterior = anterior;
+    }
+
+    public String getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(String proximo) {
+        this.proximo = proximo;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
