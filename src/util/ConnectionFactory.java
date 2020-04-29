4a56725ff724765f9fdd43cda0fcd26dbe5f78bf
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-04-29 12:55:19 -0300

    Criação do proeto

---
+++ src/util/ConnectionFactory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cipri
+ */
+public class ConnectionFactory {
+    public java.sql.Connection connector(){
+        try{
+            return DriverManager.getConnection("jdbc:mysql://localhost/Pokedex_js","root","");
+        }catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }     
+    }
+}
